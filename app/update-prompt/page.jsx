a8ee7fb8b808ee0e951459bd45bde4a3e63602d2
Manish <manishgaur459@gmail.com>
2024-03-15 05:33:46 +0530

    finalized

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,37 +1,33 @@
-"use client"
+"use client";
 import { useEffect, useState } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
+import { useRouter } from "next/router"; // Import from 'next/router' instead of 'next/navigation'
+import { useFallback, Suspense } from "react"; // Import from 'react' instead of 'next/navigation'
 import Form from "@components/Form";
 
 const UpdatePrompt = () => {
   const router = useRouter();
- 
-
-  const searchParams = useSearchParams();
-  const promptId = searchParams.get("id");
-
-  const [submitting, setIsSubmitting] = useState(false);
+  const fallbackResult = useFallback();
+  const Fallback = fallbackResult.Fallback;
+  const promptId = router.query?.id;
   const [post, setPost] = useState({ prompt: "", tag: "" });
+  const [submitting, setIsSubmitting] = useState(false);
 
   useEffect(() => {
     const getPromptDetails = async () => {
       const response = await fetch(`/api/prompt/${promptId}`);
       const data = await response.json();
-
       setPost({
         prompt: data.prompt,
         tag: data.tag,
       });
     };
-    if(promptId) getPromptDetails()
-  },[promptId])
+    if (promptId) getPromptDetails();
+  }, [promptId]);
 
   const updatePrompt = async (e) => {
     e.preventDefault();
     setIsSubmitting(true);
-
-    if(!promptId) return alert("Prompt ID not found")
-
+    if (!promptId) return alert("Missing PromptId!");
     try {
       const response = await fetch(`/api/prompt/${promptId}`, {
         method: "PATCH",
@@ -40,7 +36,6 @@ const UpdatePrompt = () => {
           tag: post.tag,
         }),
       });
-
       if (response.ok) {
         router.push("/");
       }
@@ -52,7 +47,7 @@ const UpdatePrompt = () => {
   };
 
   return (
-    
+    <Suspense fallback={<Fallback />}>
       <Form
         type="Edit"
         post={post}
@@ -60,8 +55,8 @@ const UpdatePrompt = () => {
         submitting={submitting}
         handleSubmit={updatePrompt}
       />
-
+    </Suspense>
   );
 };
 
-export default UpdatePrompt
+export default UpdatePrompt;
